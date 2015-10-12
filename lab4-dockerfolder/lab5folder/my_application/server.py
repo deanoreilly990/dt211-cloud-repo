d35a2aecd8214d11d58bd98a594a3774ae45d574
Dean <c13469208@mydit.ie>
2015-10-12 18:47:00 +0300

    update

--- lab4-dockerfolder/lab5folder/my_application/server.py
+++ lab4-dockerfolder/lab5folder/my_application/server.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello(name=None):
-    return render_template('hello.html',name=name)
+@app.route("/hello")
+def hello():
+    return "hello dean "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080,debug=True)
