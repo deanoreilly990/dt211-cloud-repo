391648f9f0ba790783bd6dbc25be44e72ca811d5
Dean <c13469208@mydit.ie>
2015-10-12 18:55:08 +0300

    updated

--- lab4-dockerfolder/lab5folder/my_application/server.py
+++ lab4-dockerfolder/lab5folder/my_application/server.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 @app.route("/hello")
 def hello():
     return "hello dean "
-
+@app.route('/user/<username>')
+def show_user_profile(username)
+	return 'User %s' %username
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080,debug=True)
 
